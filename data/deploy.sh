dbcaa8bd1d89c834390d147860aa28532d7152db
JoffreydOrtoli <joffrey.dortoli@hotmail.fr>
2022-05-05 13:45:06 +0200

    <init> branch fetch API

--- data/deploy.sh
+++ data/deploy.sh
@@ -4,13 +4,17 @@
 # createuser cinevoraces
 # createdb cinevoraces -O cinevoraces
 
+# export PGUSER=cinevoraces
+# export PGPASSWORD=cinevoraces
+# export PGDATABASE=cinevoraces
+
 # sqitch init cinevoraces --engine pg
 
 # sqitch add version_1 -n "structure de bdd"
 
 # To revert (each following command can be adressed to a specific version)
-sqitch revert db:pg:cinevoraces
+# sqitch revert db:pg:cinevoraces
 # To deploy
 sqitch deploy db:pg:cinevoraces
 # To verify
-sqitch verify db:pg:cinevoraces
+# sqitch verify db:pg:cinevoraces
