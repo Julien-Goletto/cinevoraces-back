c09169bafe9a5df1b6bff0c579df440bd74ebd4a
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-06 18:20:45 +0200

    <feature> Ajout des méthodes de fetch TMDB supplémentaire, pour récupération de la liste complète.

--- API/src/external_api/fetchAPI.js
+++ API/src/external_api/fetchAPI.js
@@ -1,51 +1,96 @@
 const axios = require('axios');
 
-const fetchList = {
+
+// `https://api.themoviedb.org/3/list/${listId}?api_key=${TMDB_API_KEY}&language=fr-FR`
+
+const fetchAPI = {
+  prefix: `https://api.themoviedb.org/3`,
   /**
-   * Function to call  3 times per movie the API TMDB to get a list of 5 top films whith searchQuery
+   * Function to call the TMDB API  to get a list of 5 top films whith searchQuery
    * @param {searchQuery} String : query from user
    * @param {TMDB_API_KEY} String : user key for API
-   * @returns {Array} Movie object with properties (title, original_title, genres, language, release, runtime, countries, languages, poster, directors, actors)
+   * @returns {Array} Movie objects
    */
-  async fetchList(searchQuery, TMDB_API_KEY){
-    let moviesListPromises = [];
-    const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&language=fr-FR&include_adult=false&query=${searchQuery}`);
-    const shortenedResponse = response.data.results.slice(0,5);
-    for (const movie of shortenedResponse) {
-      const movieDetails = await axios.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${TMDB_API_KEY}&language=fr-FR&include_adult=false`);
-      const castAndDirectors = await axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${TMDB_API_KEY}&language=fr-FR&include_adult=false`);
-      const crew = castAndDirectors.data.crew;
-      const cast = castAndDirectors.data.cast;
-      const movieGenres = movieDetails.data.genres;
-      const productCoutries = movieDetails.data.production_countries;
-      const spokenLanguages = movieDetails.data.spoken_languages;
-      directors = [];
-      crew.map((director) => {
-        if (director.job === 'Director') {
-          directors.push(director.name);
-        };
-      });
-      casting = [];
-      cast.slice(0, 5).map((actor) => {
-          casting.push(actor.name);
-      });
-      genres = [];
-      movieGenres.map((genre) => {
-        genres.push(genre.name);
-      });
-      countries = [];
-      productCoutries.map((country) => {
-        countries.push(country.name);
-      });
-      languages = [];
-      spokenLanguages.map((language) => {
-        languages.push(language.name);
-      });
-      moviesListPromises.push({french_title: movie.title, original_title: movie.original_title, genres, language: movie.original_language, release: movie.release_date, runtime: movieDetails.data.runtime, countries, languages, poster_url: movie.poster_path, directors, casting});
+  async fetchTop5Results(searchQuery, TMDB_API_KEY){
+    const response = await axios.get(`${fetchAPI.prefix}/search/movie?api_key=${TMDB_API_KEY}&language=fr-FR&include_adult=false&query=${searchQuery}`);
+    return (await response.data.results.slice(0,5));
+  },
+
+  /**
+   * Function to call the TMDB API to get a list of 5 top films whith searchQuery, fully detailled
+   * @param {searchQuery} String : query from user
+   * @param {TMDB_API_KEY} String : user key for API
+   * @returns {Array} Movie objects with properties (title, original_title, genres, language, release, runtime, countries, languages, poster, directors, actors)
+   */
+  async fetchTop5ResultsWithDetails(searchQuery,TMDB_API_KEY){
+    const movies = await fetchAPI.fetchTop5Results(searchQuery, TMDB_API_KEY);
+    const detailledMoviesPromises = [];
+    for (const movie of movies){
+      const detailledMovie = await fetchAPI.getMovieDetails(movie,TMDB_API_KEY);
+      detailledMoviesPromises.push(detailledMovie);
     };
-    const moviesList = await Promise.all(moviesListPromises);
-    return moviesList;
+    const detailledMovie = await Promise.all(detailledMoviesPromises);
+    return detailledMovie;
+  },
+
+  async fetchTMDBList(listId, TMDB_API_KEY){
+    const results = await axios.get(`${fetchAPI.prefix}/list/${listId}?api_key=${TMDB_API_KEY}&language=fr-FR`);
+    return results.data.items
+  },
+
+  async fetchTMDBListWithDetails(listId, TMDB_API_KEY){
+    const results = await fetchAPI.fetchTMDBList(listId, TMDB_API_KEY);
+    const detailledMoviesPromises = [];
+    for (const movie of results){
+      const detailledMovie = await fetchAPI.getMovieDetails(movie,TMDB_API_KEY);
+      detailledMoviesPromises.push(detailledMovie);
+    };
+    const detailledMovie = await Promise.all(detailledMoviesPromises);
+    return detailledMovie;
+  },
+  
+  /**
+   * Function to call the TMDB API  to enhance a movie initially fetched
+   * @param {movie} Object : movie object
+   * @param {TMDB_API_KEY} String : user key for API
+   * @returns {movieWithDetails} Movie object with properties : (title, original_title, genres, language, release, runtime, countries, languages, poster, directors, actors)
+   */
+  async getMovieDetails(movie,TMDB_API_KEY){
+    const movieDetails = await axios.get(`${fetchAPI.prefix}/movie/${movie.id}?api_key=${TMDB_API_KEY}&language=fr-FR&include_adult=false`);
+    const castAndDirectors = await axios.get(`${fetchAPI.prefix}/movie/${movie.id}/credits?api_key=${TMDB_API_KEY}&language=fr-FR&include_adult=false`);
+    const crew = castAndDirectors.data.crew;
+    const cast = castAndDirectors.data.cast;
+    const movieGenres = movieDetails.data.genres;
+    const productCoutries = movieDetails.data.production_countries;
+    const spokenLanguages = movieDetails.data.spoken_languages;
+    const directors = [];
+    crew.map((director) => {
+      if (director.job === 'Director') {
+        directors.push(director.name);
+      };
+    });
+    const casting = [];
+    cast.slice(0, 5).map((actor) => {
+        casting.push(actor.name);
+    });
+    const genres = [];
+    movieGenres.map((genre) => {
+      genres.push(genre.name);
+    });
+    const countries = [];
+    productCoutries.map((country) => {
+      countries.push(country.name);
+    });
+    const languages = [];
+    spokenLanguages.map((language) => {
+      languages.push(language.name);
+    });
+    const movieWithDetails = 
+      { french_title: movie.title, original_title: movie.original_title, genres,
+      language: movie.original_language, release: movie.release_date, runtime: movieDetails.data.runtime,
+      countries, languages, poster_url: movie.poster_path, directors, casting };
+    return movieWithDetails;
   },
 };
 
-module.exports = fetchList;
+module.exports = fetchAPI;
--- data/insert_movies.js
+++ data/insert_movies.js
@@ -1,12 +1,29 @@
-const fetchList = require('../API/src/external_api/fetchAPI');
+const fetchAPI = require('../API/src/external_api/fetchAPI');
 require('dotenv').config({path: '../API/.env'});
 const {TMDB_API_KEY} = process.env;
 
 const searchQuery = 'Fight Club';
+const listId = 8201424;
 
-async function test (){
-  const movies = await fetchList.fetchList(searchQuery,TMDB_API_KEY);
+async function fetch5TopMoviesFromQuery (){
+  const movies = await fetchAPI.fetchTop5Results(searchQuery,TMDB_API_KEY);
   return movies;
 }
 
-test().then(result => console.log(result));
+async function fetch5TopMoviesFromQueryWithDetails() {
+  const movies = await fetchAPI.fetchTop5ResultsWithDetails(searchQuery,TMDB_API_KEY);
+  return movies;
+}
+async function fetchTMDBList() {
+  const movies = await fetchAPI.fetchTMDBList(listId,TMDB_API_KEY);
+  return movies;
+}
+async function fetchTMDBListWithDetails() {
+  const movies = await fetchAPI.fetchTMDBListWithDetails(listId,TMDB_API_KEY);
+  return movies;
+}
+
+// fetch5TopMoviesFromQuery().then(result => console.log(result));
+// fetch5TopMoviesFromQueryWithDetails().then(result => console.log(result));
+// fetchTMDBList().then(result => console.log(result));
+fetchTMDBListWithDetails().then(result => console.log(result));
