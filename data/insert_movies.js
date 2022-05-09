89f44d5075a5a9007271afbfc1e3f426d387f2ea
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-09 11:15:57 +0200

    <feature>Sauvegarde des archives de films en .json

--- data/insert_movies.js
+++ data/insert_movies.js
@@ -2,6 +2,8 @@ const fetchAPI = require('../API/src/external_api/fetchAPI');
 require('dotenv').config({path: '../API/.env'});
 const {TMDB_API_KEY} = process.env;
 
+const { appendFile } = require('fs/promises');
+
 const searchQuery = 'Fight Club';
 const listId = 8201424;
 
@@ -20,10 +22,36 @@ async function fetchTMDBList() {
 }
 async function fetchTMDBListWithDetails() {
   const movies = await fetchAPI.fetchTMDBListWithDetails(listId,TMDB_API_KEY);
+  // return (await Promise.all(movies));
   return movies;
+}
+/**
+ * Sauvegarde le listing de films aux infos complètes dans un JSON
+ */
+async function saveMoviesList (){
+  const fileName = "moviesList.json";
+  const movies = await fetchTMDBListWithDetails();
+  const moviesLength = movies.length;
+  await appendFile(__dirname+"/"+fileName, `{"movies": [`+ `\n`);
+  let index = 1;
+  for (const movie of movies){
+    console.log(`Ajout du film ${index}: `+ movie.french_title);
+    if (index != moviesLength){
+      await appendFile(__dirname + "/" + fileName, `{"${index}": ` + `\n`
+      + JSON.stringify(movie) + `},` + `\n`);
+    }
+    else{
+      await appendFile(__dirname + "/" + fileName, `{"${index}": ` + `\n`
+      + JSON.stringify(movie) + `}` + `\n`);
+    }
+    index++;
+  };
+
+  await appendFile(__dirname + "/" + fileName, `]}`);
 }
 
 // fetch5TopMoviesFromQuery().then(result => console.log(result));
 // fetch5TopMoviesFromQueryWithDetails().then(result => console.log(result));
 // fetchTMDBList().then(result => console.log(result));
-fetchTMDBListWithDetails().then(result => console.log(result));
+// fetchTMDBListWithDetails().then(result => console.log(result));
+saveMoviesList();
