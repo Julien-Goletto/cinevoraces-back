a59b7ed811a89afe7e2ae410565bfb293aae7e7f
JoffreydOrtoli <joffrey.dortoli@hotmail.fr>
2022-05-09 18:18:04 +0200

    <fix> user datammaper number of fields

--- API/src/database/models/users.datamapper.js
+++ API/src/database/models/users.datamapper.js
@@ -17,7 +17,7 @@ const usersDataMapper = {
     const salt = await bcrypt.genSalt(10);
     const encryptedPassword = await bcrypt.hash(password,salt);
     const query = {
-      text : `INSERT INTO "user"("pseudo", "mail", "password", "role") VALUES ($1,$2,$3)`,
+      text : `INSERT INTO "user"("pseudo", "mail", "password") VALUES ($1,$2,$3)`,
       values:[pseudo,mail,encryptedPassword]
     };
     const results = await client.query(query);
