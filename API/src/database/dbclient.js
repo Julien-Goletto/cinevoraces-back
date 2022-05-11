781bd3c786e19f367307410007c424f3b7074b18
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-11 20:30:20 +0200

    <fix> nouveau test de fix client db.

--- API/src/database/dbclient.js
+++ API/src/database/dbclient.js
@@ -1,13 +1,12 @@
 const {Pool} = require('pg');
-const debug = require('debug')('DB_Client');
 
 const { DB_NAME, DB_USER, DB_HOST, DB_PORT, DB_PW } = process.env;
 const clientConfig = process.env.DATABASE_URL || { database: DB_NAME,user: DB_USER,host: DB_HOST,port: DB_PORT,password: DB_PW };
 
-const client = new Pool (clientConfig, {ssl:{ rejectUnauthorized: true }});
+const client = new Pool ({connectionString: clientConfig, ssl:{ rejectUnauthorized: true }});
 
 client.connect()
-  .then( () => debug('DB connection is live.'))
-  .catch((err) => debug('DB connection failed.', err));
+  .then( () => console.log('DB connection is live.'))
+  .catch((err) => console.log('DB connection failed.', err));
 
 module.exports = client;
