0b4e3c586537c20e990d9f1cc0ccc0c3dcf9d041
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-11 20:33:05 +0200

    <fix> Revert le dernire changement

--- API/src/database/dbclient.js
+++ API/src/database/dbclient.js
@@ -3,7 +3,7 @@ const {Pool} = require('pg');
 const { DB_NAME, DB_USER, DB_HOST, DB_PORT, DB_PW } = process.env;
 const clientConfig = process.env.DATABASE_URL || { database: DB_NAME,user: DB_USER,host: DB_HOST,port: DB_PORT,password: DB_PW };
 
-const client = new Pool ({connectionString: clientConfig, ssl:{ rejectUnauthorized: true }});
+const client = new Pool (clientConfig, {ssl:{ rejectUnauthorized: true }});
 
 client.connect()
   .then( () => console.log('DB connection is live.'))
