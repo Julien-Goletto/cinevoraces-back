5a9c072624ebf7b26bef9018489bf84aed09743b
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-12 09:44:38 +0200

    <fix> Modification du client (encore)

--- API/src/database/dbclient.js
+++ API/src/database/dbclient.js
@@ -6,18 +6,16 @@ const PG_URL = process.env.DATABASE_URL;
 // Si PGURL fournie, on crée un clientConfig avec PG_URL, sinon la même chose mais avec un objet
 let clientConfig = {};
 if (PG_URL){
-  clientConfig.connectionString = PG_URL;
+  clientConfig = { connectionString: PG_URL, ssl:{ rejectUnauthorized: false }};
 } else{
-  clientConfig = { database:DB_NAME,user:DB_USER,host:DB_HOST,port:DB_PORT,password:DB_PW };
-}
+  clientConfig = { database:DB_NAME,user:DB_USER,host:DB_HOST,port:DB_PORT,password:DB_PW }, { ssl:{ rejectUnauthorized: false }};
+};
 
 // const client = new Pool (
 //   {clientConfig},
 //   { ssl:{ rejectUnauthorized: false }}
 // );
-const client = new Pool (clientConfig,
-  { ssl:{ rejectUnauthorized: false }}
-);
+const client = new Pool (clientConfig);
 
 client.connect()
   .then( () => console.log('DB connection is live.'))
