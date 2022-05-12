b531fcbbbbf8bf6de13a735e011362fee3e1a569
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-12 09:53:07 +0200

    <fix> Reprise du client validée, fonctionne en local et distant, pull request demandée

--- API/src/database/dbclient.js
+++ API/src/database/dbclient.js
@@ -3,7 +3,6 @@ const {Pool} = require('pg');
 const { DB_NAME, DB_USER, DB_HOST, DB_PORT, DB_PW } = process.env;
 const PG_URL = process.env.DATABASE_URL;
 
-// Si PGURL fournie, on crée un clientConfig avec PG_URL, sinon la même chose mais avec un objet
 let clientConfig = {};
 if (PG_URL){
   clientConfig = { connectionString: PG_URL, ssl:{ rejectUnauthorized: false }};
@@ -11,10 +10,6 @@ if (PG_URL){
   clientConfig = { database:DB_NAME,user:DB_USER,host:DB_HOST,port:DB_PORT,password:DB_PW }, { ssl:{ rejectUnauthorized: false }};
 };
 
-// const client = new Pool (
-//   {clientConfig},
-//   { ssl:{ rejectUnauthorized: false }}
-// );
 const client = new Pool (clientConfig);
 
 client.connect()
