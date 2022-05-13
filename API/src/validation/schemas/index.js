a40db1301ff33e7d2e4d5afe2a0bade4127ade6f
JoffreydOrtoli <joffrey.dortoli@hotmail.fr>
2022-05-13 15:14:44 +0200

    <fix> road update user

--- API/src/validation/schemas/index.js
+++ API/src/validation/schemas/index.js
@@ -2,6 +2,7 @@ const moviesSchema = require('./movie.schema');
 const reviewSchema = require('./review.schema');
 const createUserSchema = require('./createuser.schema');
 const userLoginSchema = require('./userlogin.schema');
+const userUpdateSchema = require('./userupdate.schema');
 const genreSchema = require('./genre.schema');
 const languageSchema = require('./language.schema');
 const countrySchema = require('./country.schema');
@@ -12,6 +13,7 @@ module.exports = {
   reviewSchema,
   createUserSchema,
   userLoginSchema,
+  userUpdateSchema,
   genreSchema,
   languageSchema,
   countrySchema,
