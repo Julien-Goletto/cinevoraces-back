d51ec2de6474452f3ed95bfd209c8027e9649372
anthonyespirat <jayshu666@gmail.com>
2022-05-16 17:22:06 +0200

    <fix> data send by log & refresh

--- API/src/controllers/users.controller.js
+++ API/src/controllers/users.controller.js
@@ -14,14 +14,8 @@ const usersController = {
     const result = await usersDataMapper.logUser(user);
     const accessToken = jwtMethods.createAccessToken(result);
     const refreshToken = jwtMethods.createRefreshToken(result);
-    res.cookie('accessToken', accessToken, {
-      httpOnly: true,
-      sameSite: 'none',
-    });
-    res.cookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      sameSite: 'none',
-    });
+    res.cookie('accessToken', accessToken);
+    res.cookie('refreshToken', refreshToken);
     res.status(200).json(result);
   },
 
