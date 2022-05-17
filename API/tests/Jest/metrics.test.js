cbe2ca1397708277dc53e64f4e51c8cface19c3a
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-17 10:50:00 +0200

    <fix> metrics by userId

--- API/tests/Jest/metrics.test.js
+++ API/tests/Jest/metrics.test.js
@@ -5,28 +5,27 @@ const session = require('supertest-session');
 const app = require('../../app');
 
 const request = supertest(app);
-const testSession = session(app);
+const userSession = session(app);
 
 const registeredUser = { pseudo: 'Mat-Mat', password: 'e5BkKI_rG5fg_6l!qG1I' };
 
 describe('API e2e', () => {
   describe('Metrics routes', () => {
+    beforeAll(async () => {
+      await userSession.post('/v1/users/login').send(registeredUser);
+    });
     it('Should get general metrics', async () => {
       const response = await request.get('/v1/metrics/');
       expect(response.status).toBe(200);
       expect(response.text).toContain('seasons_count');
     });
-    it('Should log a user', async () => {
-      const response = await testSession.post('/v1/users/login').send({ ...registeredUser });
-      expect(response.status).toBe(200);
-    });
     it('Should get all individual metrics', async () => {
-      const response = await testSession.get('/v1/metrics/all');
+      const response = await userSession.get('/v1/metrics/all');
       expect(response.status).toBe(200);
       expect(response.text).toContain('proposed_movies_count');
     });
     it('Should get individual metrics for user n°1', async () => {
-      const response = await testSession.get('/v1/metrics/1');
+      const response = await userSession.get('/v1/metrics/1');
       expect(response.status).toBe(200);
       expect(response.text).toContain('proposed_movies_count');
     });
