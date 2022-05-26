ab4570ba9316a253dc31ebd6477296fa9171efff
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-26 16:18:17 +0200

    Ajout du module cloudinary

---
+++ API/src/external_api/cloudinary.module.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
+
+const cloudinaryUpload = {
+  options: {
+    folder: 'cinevoraces',
+    tags: 'avatar',
+    width: 100,
+    height: 100,
+    crop: 'fill',
+    gravity: 'faces',
+    format: 'jpg',
+  },
+  async uploadThumbnail(CLOUDINARY_URL, userPseudo, sourceImage) {
+    cloudinary.config({
+      cloudinary_url: CLOUDINARY_URL,
+    });
+    const { options } = cloudinaryUpload;
+    options.public_id = userPseudo;
+    const upload = await cloudinary.uploader.upload(sourceImage, options);
+    console.log(upload);
+    return upload.url;
+  },
+};
+
+module.exports = cloudinaryUpload;
