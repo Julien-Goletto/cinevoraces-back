9d91cf17366cb5fb287355cdd24ec1e5e2c98d65
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-26 19:23:10 +0200

    un peu de seeding en plus

--- data/deploy/version_4.sql
+++ data/deploy/version_4.sql
@@ -43,7 +43,15 @@ INSERT INTO "user" ("pseudo","mail","password") VALUES
 ('olive qui tue', 'mailbidon33' ,'$2b$10$hOwvB1jgPGt7F5mkNxz1kO8vESSC7lw9HWJ/JeokniuMjnU0Aaer6');
 
 -- Ajout des photos de profil
-UPDATE "user" SET
+UPDATE "user" SET avatar_url = 'http://res.cloudinary.com/yvessignal/image/upload/v1653584757/cinevoraces/Calys.jpg' WHERE pseudo='Calys';
+UPDATE "user" SET avatar_url = 'http://res.cloudinary.com/yvessignal/image/upload/v1653584757/cinevoraces/Checco.jpg' WHERE pseudo='Checco';
+UPDATE "user" SET avatar_url = 'http://res.cloudinary.com/yvessignal/image/upload/v1653584757/cinevoraces/Klavor.jpg' WHERE pseudo='Klavor';
+UPDATE "user" SET avatar_url = 'http://res.cloudinary.com/yvessignal/image/upload/v1653584757/cinevoraces/Lukino.jpg' WHERE pseudo='Lukino';
+UPDATE "user" SET avatar_url = 'http://res.cloudinary.com/yvessignal/image/upload/v1653584757/cinevoraces/Mat-Mat.jpg' WHERE pseudo='Mat-Mat';
+UPDATE "user" SET avatar_url = 'http://res.cloudinary.com/yvessignal/image/upload/v1653584757/cinevoraces/Morbo.jpg' WHERE pseudo='Morbo';
+UPDATE "user" SET avatar_url = 'http://res.cloudinary.com/yvessignal/image/upload/v1653584757/cinevoraces/Sao.jpg' WHERE pseudo='Sao';
+UPDATE "user" SET avatar_url = 'http://res.cloudinary.com/yvessignal/image/upload/v1653584757/cinevoraces/Yves%20Signal' WHERE pseudo='Yves Signal';
+UPDATE "user" SET avatar_url = 'http://res.cloudinary.com/yvessignal/image/upload/v1653584757/cinevoraces/olive%20qui%20tue.jpg' WHERE pseudo='olive qui tue';
 
 -- Passage de Yves Signal en admin
 UPDATE "user"
@@ -416,62 +424,14 @@ SET is_published = 'true';
 
 -- Ajout de reviews
 INSERT INTO "review" ("user_id","movie_id","bookmarked","viewed","liked","rating","comment") VALUES
-(1,1,false,true,true,5,'Un grand merci à Wade pour cette proposition. C''est la deuxième fois que je le vois et, pour résumer, je l''ai trouvé encore plus puissant. Je vais essayer d''être pertinent et pas trop brouillon mais
- ce film m''a retourné donc je ne garantis pas la cohérence de cette "critique". L''avantage de revoir un film c''est qu''on évacue les grandes lignes du scénario et qu''on se focalise sur des détails passés inaperçus la première fois.
-Le film est servi par deux très grands acteurs. Et il n''en fallait pas moins pour sublimer la grande force de Jeff Nichols : faire avancer l''intrigue et transmettre des émotions par des silences et des regards. 
-Les silences de Curtis dans la première partie du film d''abord, un Curtis qui sent son monde s''effondrer sous ses pieds à cause d''une menace dont il ne sait si elle est intérieure ou s''il est le seul à vraiment la discerner.
-C''est d''ailleurs une des forces du film : les visions se font de plus en plus angoissantes mais les explications rationnelles arrivent en écho (la maladie de se mère, la crise d''angoisse, l’irrationalité qui mènera au retard 
-de traitement pour sa fille). Puis, dans la dernière demi-heure (grandiose), les silences et les regards se font, là aussi signifiants mais la famille y est associée. Lors du repas du Lion''s Club notamment où deux regards se répondent :
- celui de Curtis qui semble crier à sa fille qu''il a peur pour elle, qu''il est sûrement fou mais qu''il l''aime et qu''il sera toujours là et celui de Samantha qui comprend et qui l''assure de son soutien. Bref, cette scène m''a anéanti.
- Je considère Jeff Nichols comme un des plus grands : Shotgun Stories, Mud, Midnight Special sont de grands films. Mais j''ai une affection toute particulière pour celui-ci car les sujets sont nombreux et tous bien traités. 
- D''abord la parentalité. Ce n''est d''ailleurs sûrement pas un hasard si ce film m''a encore plus touché aujourd''hui.
-Curtis a peur, peur de lui, peur pour sa famille, peur de ne pas être à la hauteur. Il doit agir et la sortie de l''abri lui donne une "réponse" à ses peurs : C''est normal de perdre pied. Curtis veut tout gérer pour sa famille
- mais c''est impossible et il se réfugie dans la peur et le repli sur soi symbolisé par ce bunker. Mais il doit agir, prendre conscience, avancer. Ok certains y verront de gros sabots mais pour moi c''est une superbe métaphore.
-Le film traite aussi d''héritage, celui de la mère de Curtis notamment, du couple, confronté à la parentalité et au handicap, de la maladie et de la conscience d''être malade. J''ajouterai aussi le parallèle qui ne paraissait
-   pas si évident lors du premier visionnage, celui de la catastrophe annoncée, du cataclysme minimisé (même si les visions de Curtis n''ont pas toutes trait à des perturbations climatiques). Enfin, ce film s''inscrit dans une
-filmographie qui rend hommage à une certaine Amérique. Celle qu''on a caricaturé après l''élection de l''homme orange. Celle qui compte en son sein des communautés rurales et aimantes. 
-Pour la petite anecdote, la dernière chanson du film (de la country évidemment) est interprétée par le frère de Jeff Nichols, Ben, et résume parfaitement ce qu''on vient de voir.
-Merci encore...'),
-(2,2,false,true,false,2,'C tro nul ce truc. c un film ca ? allai renbale la sauce et rentre ché toi et bisou'),
-(1,3,true,true,true,4,'Un grand merci à Wade pour cette proposition. C''est la deuxième fois que je le vois et, pour résumer, je l''ai trouvé encore plus puissant. Je vais essayer d''être pertinent et pas trop brouillon mais
- ce film m''a retourné donc je ne garantis pas la cohérence de cette "critique". L''avantage de revoir un film c''est qu''on évacue les grandes lignes du scénario et qu''on se focalise sur des détails passés inaperçus la première fois.
-Le film est servi par deux très grands acteurs. Et il n''en fallait pas moins pour sublimer la grande force de Jeff Nichols : faire avancer l''intrigue et transmettre des émotions par des silences et des regards. 
-Les silences de Curtis dans la première partie du film d''abord, un Curtis qui sent son monde s''effondrer sous ses pieds à cause d''une menace dont il ne sait si elle est intérieure ou s''il est le seul à vraiment la discerner.
-C''est d''ailleurs une des forces du film : les visions se font de plus en plus angoissantes mais les explications rationnelles arrivent en écho (la maladie de se mère, la crise d''angoisse, l’irrationalité qui mènera au retard 
-de traitement pour sa fille). Puis, dans la dernière demi-heure (grandiose), les silences et les regards se font, là aussi signifiants mais la famille y est associée. Lors du repas du Lion''s Club notamment où deux regards se répondent :
- celui de Curtis qui semble crier à sa fille qu''il a peur pour elle, qu''il est sûrement fou mais qu''il l''aime et qu''il sera toujours là et celui de Samantha qui comprend et qui l''assure de son soutien. Bref, cette scène m''a anéanti.
- Je considère Jeff Nichols comme un des plus grands : Shotgun Stories, Mud, Midnight Special sont de grands films. Mais j''ai une affection toute particulière pour celui-ci car les sujets sont nombreux et tous bien traités. 
- D''abord la parentalité. Ce n''est d''ailleurs sûrement pas un hasard si ce film m''a encore plus touché aujourd''hui.
-Curtis a peur, peur de lui, peur pour sa famille, peur de ne pas être à la hauteur. Il doit agir et la sortie de l''abri lui donne une "réponse" à ses peurs : C''est normal de perdre pied. Curtis veut tout gérer pour sa famille
- mais c''est impossible et il se réfugie dans la peur et le repli sur soi symbolisé par ce bunker. Mais il doit agir, prendre conscience, avancer. Ok certains y verront de gros sabots mais pour moi c''est une superbe métaphore.
-Le film traite aussi d''héritage, celui de la mère de Curtis notamment, du couple, confronté à la parentalité et au handicap, de la maladie et de la conscience d''être malade. J''ajouterai aussi le parallèle qui ne paraissait
-   pas si évident lors du premier visionnage, celui de la catastrophe annoncée, du cataclysme minimisé (même si les visions de Curtis n''ont pas toutes trait à des perturbations climatiques). Enfin, ce film s''inscrit dans une
-filmographie qui rend hommage à une certaine Amérique. Celle qu''on a caricaturé après l''élection de l''homme orange. Celle qui compte en son sein des communautés rurales et aimantes. 
-Pour la petite anecdote, la dernière chanson du film (de la country évidemment) est interprétée par le frère de Jeff Nichols, Ben, et résume parfaitement ce qu''on vient de voir.
-Merci encore...'),
-(1,4,false,true,true,5,'Le film commence fort, avec une belle baston reprenant les bruits typiques d’un Bud Spencer et Terence Hill, avec l’air de musique de la Cantina de Star Wars, hilarité totale pour ma part ! 
-Par la suite, j’ai finalement trouvé la musique assez bonne, variée, rythmée et originale. Les chants et chorégraphie sympa (même si j’en ai passés certains vers la fin en accélérée). 
-Les cascades improbables m’ont bien fait marrer, ainsi que le méchant, avec sa tête de vainqueur qui ne peut juste pas faire peur. 
-Les courses poursuites en voitures ont un rendu des plus étranges… et le tout me semble presque d’être un film expérimental. 
-Des fois, ça donne des bonnes surprises, et des fois, c’est totalement raté. J’ai particulièrement apprécié les plans sur la vie courante indienne, avant de revenir par un dézoom 
-à une scène de course poursuite. 
-Maintenant les gros hics : finalement le film ne parle pas vraiment de Kabaddi, et cela, ça me rend triste. C’est juste un prétexte pour donner le l’attrait au film. 
-Autre chose, le scénario est vraiment old school et patriarcal au possible : un homme qui défie un gros méchant pour sauver une fille sans défense cucul la praline. 
-C’est pourri. Bon, maintenant on est en Inde, ça reste pourri, mais je suis un peu plus tolérant que si ça avait été une production occidentale. 
-Et 3h00 pour un tel scénario post-it, ce n’est juste pas possible ! On aurait très facilement pu retirer 1h30. En tout cas, pour l’instant, le meilleur film que j’ai vu de la S3 de cinévorace, 
-merci Lukino pour cette reco exotique. Et aussi pour le jambon.'),
-(2,3,true,false,false,null,null);
+(9,107,'false','true','false',1,'Je n’ai jamais vu un film avec autant de plans différents qui se suivent, à s’en rendre malade, d’autant plus qu’une majorité fait moins de 3 sec, et lors des scènes d’actions ça s’enchaîne en pagaille. Ça en devient totalement incompréhensible, et ça épuise. Sinon niveau scénar, on est en plein série Z (les gens meurent de faim, et en parallèle, je n’ai jamais vu un plan avec autant de vaches), mais dans le fond, je trouve l’idée assez marrante, que l’Écosse soit isolée et que du coup plusieurs civilisations différentes apparaissent, ça aurait pu être sympa l’idée, avec chacune leur dogme, objectifs, etc. Évidement ça a été traité à l’arrache, avec une peuplade post-apo à la mad max (copié-collé) et l’autre en Moyen-âge avec les clichés qui vont avec. Les acteurs et actrices sont mauvais, le script aussi, et Bentley ont dû financer le film (pour le meilleur et surtout pour le pire), ce n’est pas possible autrement. En fait, ce film permet de se rendre compte ce qu’aurait donné un très mauvais MadMax Fury Road et donne envie de se le remâter (MadMax donc !).'),
+(32,107,'false','true','false',2,'Si Doomsday avait été un jeu vidéo j''aurais sûrement adoré y jouer ! Les environnements sont beaux et variés et les phases de combat ont l''air stylées ! Bon, comme dans la plupart des jeux le scénario ne casse pas des briques, les acteurs jouent mal, les ennemis comme les coéquipiers sont un peu stupides et le gameplay de la phase en voiture est un peu raté, mais on a l''habitude... Le plus gros problème c''est que Neil Marshall n''a pas voulu me laisser la manette et qu''en plus il a pas arrêté d''appuyer toutes les deux secondes sur le bouton pour changer la caméra...');
 
 --Ajout des créneaux
 --2022-06-26T22:00:00.000Z
 INSERT INTO proposition_slot ("season_number","episode","publishing_date","is_booked") VALUES
-(3,19,'2022-05-02',true),
-(3,20,'2022-05-09',true),
-(3,21,'2022-05-16',true),
-(3,22,'2022-05-23',true),
-(3,23,'2022-05-30',true),
-(3,24,'2022-06-06',false),
-(3,25,'2022-06-13',false),
+(3,24,'2022-06-06',true),
+(3,25,'2022-06-13',true),
 (3,26,'2022-06-20',false),
 (3,27,'2022-06-27',false),
 (3,28,'2022-07-04',false),
