566d61f2a41a9292cc61ffaa5b401c7c61f92eaa
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-27 09:44:49 +0200

    repassage de la durée de vie du AT à 60 s

--- API/src/JWT/jwt.module.js
+++ API/src/JWT/jwt.module.js
@@ -5,7 +5,7 @@ const ACCESS_TOKEN_SECRET = `${process.env.ACCESS_TOKEN_SECRET}`;
 const REFRESH_TOKEN_SECRET = `${process.env.REFRESH_TOKEN_SECRET}`;
 
 const jwtATConfig = {
-  expiresIn: 5,
+  expiresIn: 60,
   algorithm: 'HS256',
 };
 const jwtRTConfig = {
