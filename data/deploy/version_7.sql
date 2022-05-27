5afc517bb633c16e0ac04752d59a6648ec0a576d
Julien GOLETTO <julien.goletto@gmail.com>
2022-05-27 10:42:43 +0200

    ajout d'un peu de seeding en plus

--- data/deploy/version_7.sql
+++ data/deploy/version_7.sql
@@ -6,7 +6,15 @@ BEGIN;
 INSERT INTO "user" ("pseudo","mail","password") VALUES
 ('Joffrey d''Ortoli', 'mailbidon34' ,'$2b$10$fNwqHYQdB8z2TYljQTAU6OnL8BqtOH.wn8MIOzzpj2VwsE.Y9FmcS'),
 ('Benoît Safari', 'mailbidon35' ,'$2b$10$z6hd1aqv7aRAIX1Jvw8sIONcaMDtSV2M2UkDYJJEipQXANR9q5L4W'),
+('nox', 'mailbidon36@net.fr' ,'$2a$10$pVvqSjJWiPa.hsGuAWbPNeX0A10Dtz4/PpGwAXQOWMuLZxIzQAD2m'),
+('benoitSafari', 'mailbidon37@mail.net' ,'$2a$10$/BlUX4DDVkuC63TbC6JsyeTMQoduAcLr52WSBp/CTBYh2hOI6kiEG'),
+('Mahh31', 'mailbidon38@mail.net' ,'$2a$10$YWs6cq02Gx/OHcuQL0xiqOYn8qc5g3EWyUAUDHhKmufu2iTC10zlS'),
 ('Jean Test', 'mailbidon36' ,'$2b$10$IK5jJO8ntnu.nbyGTHVG6uXYhuWDt0ZFfVSQmEQHkHbu0L5sl9/42');
+
+-- Ajout des PP
+UPDATE "user" SET avatar_url = 'https://images-ext-1.discordapp.net/external/GVEsPmcMp4X4PuWDDbw6VOfeHQS-994SugEXb3rG9_g/%3Fv%3D4/https/avatars.githubusercontent.com/u/72048119' WHERE pseudo='nox';
+UPDATE "user" SET avatar_url = 'https://media.discordapp.net/attachments/968514181785608232/979665534079615057/unknown.png' WHERE pseudo='benoitSafari';
+UPDATE "user" SET avatar_url = 'https://media.discordapp.net/attachments/968514181785608232/979649375414415380/Skeleton_black_white.jpg?width=336&height=686' WHERE pseudo='benoitSafari';
 
 -- Ajout de deux nouvelles propositions
 SELECT new_movie('12 hommes en colère','12 Angry Men','https://image.tmdb.org/t/p/original/fFXrCl7nBFFaQU3IgTlinvk6vTi.jpg',array['Sidney Lumet'],'1957-04-10','95',array['Martin Balsam','John Fiedler','Lee J. Cobb','E.G. Marshall','Jack Klugman'],'12 hommes et beaucoup de colère. Mais zéro bagarre par contre. Même pas une tartine de pâté. C''est rude.','2022-06-06',34,3,array['Drame'],array['English'],array['United States of America']);
