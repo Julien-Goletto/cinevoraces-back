b3a345080f9c50ac53b44a34ef4bcdfcf55394f1
Julien GOLETTO <julien.goletto@gmail.com>
2022-06-16 11:35:59 +0200

    correction de la vue filters_options

--- data/deploy/version_8.sql
+++ data/deploy/version_8.sql
@@ -5,24 +5,24 @@ BEGIN;
 DROP VIEW filters_options;
 
 CREATE VIEW filters_options AS
-  SELECT 
+      SELECT 
       sl.seasons_list,
       gl.genres_list,
       cl.countries_list,
       rdl.min_max_dates,
       mr.max_runtime
-    FROM 
-      ( SELECT array_agg(DISTINCT season.number ||' - '|| season.year) AS seasons_list
+      FROM 
+      ( SELECT array_agg(DISTINCT array[season.number,season.year] ) AS seasons_list
             FROM season) sl,
       ( SELECT array_agg(DISTINCT genre.name) AS genres_list
             FROM genre) gl,
       ( SELECT array_agg(DISTINCT country.name) AS countries_list
             FROM country) cl,
-      ( SELECT array_agg((mind.min || ','::text) || maxd.max) AS min_max_dates
+      ( SELECT array [mind.min,maxd.max]  AS min_max_dates
             FROM ( SELECT EXTRACT(YEAR FROM min(movie.release_date)) AS min
-                    FROM movie) mind,
-              ( SELECT EXTRACT(YEAR FROM max(movie.release_date)) AS max
-                    FROM movie) maxd) rdl,
+                  FROM movie) mind,
+            ( SELECT EXTRACT(YEAR FROM max(movie.release_date)) AS max
+                  FROM movie) maxd) rdl,
       ( SELECT max(movie.runtime) AS max_runtime FROM movie) mr;
 
 COMMIT;
--- data/revert/version_8.sql
+++ data/revert/version_8.sql
@@ -2,6 +2,16 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+DROP VIEW filters_options;
+
+CREATE VIEW filters_options AS
+	SELECT sl.seasons_list seasons_list, gl.genres_list genres_list, cl.countries_list countries_list,
+			dl.min_max_dates min_max_dates
+	FROM
+		(SELECT array_agg(DISTINCT number) seasons_list FROM season) sl,
+		(SELECT array_agg(DISTINCT name) genres_list FROM genre) gl,
+		(SELECT array_agg(DISTINCT name) countries_list FROM country) cl,
+		(SELECT array_agg(mind.min || ',' || maxd.max) min_max_dates
+		FROM (SELECT MIN(release_date) FROM movie) mind, (SELECT MAX(release_date) FROM movie) maxd) dl;
 
 COMMIT;
